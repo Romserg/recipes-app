98d6dd55bd77cfd1e4042a25655180d48380ada1
Romserg <serg.romankov@gmail.com>
2023-09-06 11:42:58 +0300

    Use a service for cross-component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
 import { Recipe } from "./recipe.model";
+import { EventEmitter } from "@angular/core";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('First recipe', 'test', 'https://shorturl.at/diM28'),
     new Recipe('Second recipe', 'test', 'https://shorturl.at/gjIZ6')
