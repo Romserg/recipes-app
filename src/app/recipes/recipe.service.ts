6144aa5dae764fc3e44be547715eb1a4789ea1dc
Romserg <serg.romankov@gmail.com>
2024-09-06 13:50:01 +0300

    Add image preview

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,7 +10,8 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>
   private recipes: Recipe[] = [
     new Recipe('Pizza', 'Tasty pizza', 'https://t.ly/gCe7k', [new Ingredient('Cheese', 1)]),
-    new Recipe('Burger', 'Fat burger', 'https://t.ly/aPns7', [new Ingredient('Meat', 1), new Ingredient('Buns', 2)])
+    new Recipe('Burger', 'Fat burger', 'https://t.ly/aPns7', [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]),
+    new Recipe('Spaghetti', 'Tasty spaghetti', 'http://surl.li/dwdmng', [new Ingredient('Spaghetti', 10)])
   ]
 
   constructor(private shoppingListService: ShoppingListService) {
